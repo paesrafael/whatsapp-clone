df0599a681f1eea56aac8b6552e0aa42290d7298
Rafael Paes <rfaelpaes@gmail.com>
2020-07-08 22:42:09 -0300

    :lipstick: Add styles padrão

---
+++ src/assets/styles/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.View`
+  background-color: #EDEDED;
+  flex: 1;
+`
+
+const Content = styled.View`
+  flex-direction: row;
+`
+
+const Text = styled.Text``
+
+export { Container, Content, Text }
