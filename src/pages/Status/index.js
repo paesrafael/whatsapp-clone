4528a043480427cc6c2515391e7364d574f0ae46
Rafael Paes <rfaelpaes@gmail.com>
2020-07-08 22:42:56 -0300

    :sparkles: :construction: Add componente Status

---
+++ src/pages/Status/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+  Container,
+  Text,
+} from './styles'
+
+export default function Status() {
+  return (
+    <Container>
+      <Text />
+    </Container>
+  )
+}
---
+++ src/pages/Status/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.View`
+
+`
+
+const Text = styled.Text`
+
+`
+
+export {
+  Container, Text,
+}
