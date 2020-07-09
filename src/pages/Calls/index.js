7acf184bd65e145a94e4cc8dc2544ca0a65fba0f
Rafael Paes <rfaelpaes@gmail.com>
2020-07-08 22:43:22 -0300

    :sparkles: :construction: Add componente Chamadas

---
+++ src/pages/Calls/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {
+  Container,
+  Text,
+} from './styles'
+
+export default function Calls() {
+  return (
+    <Container>
+      <Text />
+    </Container>
+  )
+}
