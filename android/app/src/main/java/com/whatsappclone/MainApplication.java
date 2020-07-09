983d7e5b2a02157d8dce652eb0266ba430043b53
Rafael Paes <rfaelpaes@gmail.com>
2020-07-09 15:57:46 -0300

    :bug: fix problema com o nome ao instalar no smartphone

--- android/app/src/main/java/com/whatsappclone/MainApplication.java
+++ android/app/src/main/java/com/whatsappclone/MainApplication.java
@@ -1,4 +1,4 @@
-package com.whatsapp;
+package com.whatsappclone;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.whatsapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.whatsappclone.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
